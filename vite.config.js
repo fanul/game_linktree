30934b24cc06cba60c28d9657aa52d208a498bf5
Mohammad Afwanul <mohammad.afwanul@bpjamsostek.local>
2026-07-30 11:37:50 +0700

    build: auto-update bundle, clasp sync and deploy

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({ plugins: [vue()], base: './' })
+export default defineConfig({
+  plugins: [vue()],
+  base: './',
+  build: {
+    target: 'es2015',
+    modulePreload: false,
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        format: 'iife',
+        name: 'GameLinktreeApp',
+        inlineDynamicImports: true
+      }
+    }
+  }
+})
