1fbbb9646ecc72f9318671edcab3af097a3cc5d4
Fanul <fanul@example.com>
2026-07-30 11:29:05 +0700

    fix: remove hash from public route URLs

--- src/router.js
+++ src/router.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import LandingPage from './views/LandingPage.vue'
 import AdminPage from './views/AdminPage.vue'
 
 export default createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [
     { path: '/', component: LandingPage },
     { path: '/admin', component: AdminPage }
